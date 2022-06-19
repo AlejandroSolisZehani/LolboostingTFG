e59dfcb9df9c3144b5bdb0fac49d75262df337cb
Alejandro Solis Zehani <alejandrosoliszehani@gmail.com>
2022-06-19 20:19:06 +0200

    puerto?

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import app from "./app.js"
 import { connectDB } from "./db.js";
 
-
+port = process.env.port || 4000
 
 
 connectDB()
 
-app.listen(4000);
+app.listen(port);
 console.log('El servidor esta corriendo en el puerto', 4000)
