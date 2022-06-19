2482bae0110b697157cf0e99b3312af5031882ee
Alejandro Solis Zehani <alejandrosoliszehani@gmail.com>
2022-06-19 17:34:32 +0200

    basico

--- lolboosting/src/Paginas/Aterrizaje/Aterrizaje.js
+++ lolboosting/src/Paginas/Aterrizaje/Aterrizaje.js
@@ -10,7 +10,13 @@ export default function Aterrizaje() {
       <div>
         Aterrizaje
       <Link to='aadasd' className="text-black-100 block">Go to 404</Link>
-     
+      <Link to='/miperfil' className="text-black-100 block">mi perfil</Link>
+      <Link to='/productos' className="text-black-100 block">productos</Link>
+      <Link to='/boost' className="text-black-100 block">boosts</Link>
+      <Link to='/maestrias' className="text-black-100 block">maestrias</Link>
+      <Link to='/login' className="text-black-100 block">login</Link>
+      <Link to='/signup' className="text-black-100 block">registrarse</Link>
+      <Link to='/cuentas' className="text-black-100 block">cuentas</Link>
       </div>
     </>
   )
--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import postsRoutes from "./routes/routes.js"
+import {dirname, join} from "path"
+import {fileURLToPath} from "url"
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
 //middleware
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 //routes
 app.use(postsRoutes)
+console.log(__dirname)
+app.use(express.static(join(__dirname, '../lolboosting/build')))
 
 export default app
