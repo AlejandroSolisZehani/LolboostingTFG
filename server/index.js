c8e3a8094cd2b908d9090827a86db925c3748fa3
Alejandro Solis Zehani <alejandrosoliszehani@gmail.com>
2022-06-19 21:39:02 +0200

    x

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import app from "./app.js"
 import { connectDB } from "./db.js";
 
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000
 
-
+const host = '0.0.0.0';
 connectDB()
 
-app.listen(port, () => {
+app.listen(port, host, () => {
    console.log("App is running on port "+ port)
   });
 console.log('El servidor esta corriendo en el puerto', port)
